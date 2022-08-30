1485bd2fd93346f6745a70f649b24d93cff37bb0
AkiraZeZero <kiara.ocasio07@gmail.com>
2022-08-30 11:49:11 -0400

    final commit

--- server/server.js
+++ server/server.js
@@ -8,4 +8,5 @@ app.use(express.static('public'))
 
 require(`./routes.js`)(app)
 
-app.listen(4000,console.log("Server running on 4000"))
+const PORT = process.env.PORT || 4000
+app.listen(PORT,console.log("Server running on port 4000"))
